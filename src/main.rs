39d1dc98672d894f80b1451b9c4fede43dac52ba
Salfa Chang <salfa@foxmail.com>
2023-08-10 13:02:02 +0800

    make a test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,20 @@
 mod sal_server;
 
 use std::collections::HashMap;
-use std::env;
 use sal_server::SalServer;
 
 fn main() {
 
-    let port = env::var("PORT").unwrap_or(String::from("8888"));
-    let mut addr = String::from("0.0.0.0:");
-    let _ = addr.push_str(port.as_str());
+    let serv = SalServer::new("0.0.0.0:8888", 8);
+    println!("Running at Port 8888");
 
-    println!("Addr: {}", addr);
-
-    let serv = SalServer::new(&addr, 8);
     serv.route_http(route);
-    println!("Hello, world!");
+
 }
 
 fn route(http_line: (&str, &str), head: HashMap<&str, &str>, body: &str) -> (Vec<u8>, bool) {
+    println!("HEADER: {:#?}", head);
+
     let mut buf = Vec::from("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n");
     let buff = Vec::from(format!("Http Line: {:?}\r\nHead: {:#?}\r\nBody: {}\r\n", http_line, head, body));
     buf.extend(buff);
