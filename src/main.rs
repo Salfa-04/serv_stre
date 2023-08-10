8598d603f1976dd01c26a99e11451dd744cd86f3
Salfa Chang <salfa@foxmail.com>
2023-08-10 14:05:34 +0800

    fix a bug

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,30 @@ use sal_server::SalServer;
 
 fn main() {
 
-    for (key, val) in env::vars() {
-        println!("{key}: {val}");
-    };
-
     let port = env::var("PORT").unwrap();
     let mut addr = String::from("0.0.0.0:");
-    let _ = addr.push_str(port.as_str());
+    addr.push_str(port.as_str());
 
-    let serv = SalServer::new(&addr, 8);
-
-    serv.route_http(route);
+    SalServer::new(&addr, 8).route_http(route);
 
 }
 
 fn route(http_line: (&str, &str), head: HashMap<&str, &str>, body: &str) -> (Vec<u8>, bool) {
-    println!("HEADER: {:#?}", head);
 
-    let mut buf = Vec::from("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n");
-    let buff = Vec::from(format!("Http Line: {:?}\r\nHead: {:#?}\r\nBody: {}\r\n", http_line, head, body));
-    buf.extend(buff);
+    if let Some(x) = head.get("Connection") {
+        println!("Keep-Alive: {}", x);
+    };
+
+    let val = if body.is_empty() {
+        format!("Http Line: {:?}\r\nHead: {:#?}\r\n", http_line, head)
+    } else {
+        format!("Http Line: {:?}\r\nHead: {:#?}\r\nBody: {}\r\n", http_line, head, body)
+    };
+
+    let mut buf = Vec::from(
+        "HTTP/1.1 200 OK\r\n\
+        Content-Type: text/plain; charset=utf-8\r\n\r\n"
+    );
+    buf.extend(Vec::from(val));
     return (buf, false)
 }
